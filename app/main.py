576264ce5dce01a6560bb787f5bc1b059f7d845a
Abdurrehman Arif <abdurrehmanarifwaqar@gmail.com>
2026-03-12 09:13:06 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from app.database import Base, engine
+
+from app.database import engine
+from app import models
+
 from app.routes import auth_routes, chat_routes
 
-# Create database tables
-Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="AI Chat System with Gemini")
+app = FastAPI()
 
-# Include routers
 app.include_router(auth_routes.router)
 app.include_router(chat_routes.router)
